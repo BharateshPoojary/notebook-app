94428404b30fc5d2a7883b44baec367ec69fa345
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-08-18 22:17:42 +0530

    navbar done and used uselocation hook

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ const App = () => {
             {/* <NoteState>*NoteState is the component to which we are passing navbar and outlet as prop. Wrapping up this component so that it can access the  contexts state . Outlet includes/refers to  all the child components like signin/up ,about etc */}
             <Navbar />
             <Outlet />
-
             {/* </NoteState> */}
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,8 @@ const Navbar = () => {
     // useEffect(() => {
     //     a.update();//We are updating a particular state by accessing the update function using useContext hook
     // }, []);
-    const location = useLocation();
+    const location = useLocation();//Returns the current location object, which represents the current URL in web browsers.
+
     useEffect(() => {
         console.log(location.pathname);
     }, [location])
@@ -19,9 +20,10 @@ const Navbar = () => {
                 {/* <h2>hi {a.state.name} and {a.state.class}</h2>* 'a'  holds the context value which is sent in our case it is state object it can be accessed */}
                 <h3>Bharat Note </h3>
                 <div className='navitems'>
-                    <NavLink className={`navlinks ${location.pathname === "/Signin" ? 'onnavlinkclick' : 'navlinks'}`} to='/Signin'>Signin</NavLink>
-                    <NavLink className={`navlinks ${location.pathname === "/Signup" ? 'onnavlinkclick' : 'navlinks'}`} to='/Signup'>Signup</NavLink>
-                    <NavLink className={`navlinks ${location.pathname === "/About" ? 'onnavlinkclick' : 'navlinks'}`} to='/About'>About</NavLink>
+                    <NavLink className={` ${location.pathname === "/" ? 'onnavlinkclick' : 'navlinks'}`} to='/'>Home</NavLink>
+                    <NavLink className={` ${location.pathname === "/Signup" ? 'onnavlinkclick' : 'navlinks'}`} to='/Signup'>Signup</NavLink>
+                    <NavLink className={` ${location.pathname === "/Signin" ? 'onnavlinkclick' : 'navlinks'}`} to='/Signin'>Signin</NavLink>
+                    <NavLink className={` ${location.pathname === "/About" ? 'onnavlinkclick' : 'navlinks'}`} to='/About'>About</NavLink>
                     <i className="ri-menu-3-fill"></i>
                 </div>
             </nav>
