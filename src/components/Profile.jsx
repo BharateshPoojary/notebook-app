5f0d0d54541cc3c389fa0681b89ff5ab39da51b6
Bharatesh Poojary <bharateshpoojari@gmail.com>
2025-02-07 23:29:30 +0530

    issue resolved

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,29 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import './Profile.css'
-const Profile = async () => {
+const Profile = () => {
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [useremail, setUseremail] = useState('');
 
+    useEffect(async () => {
 
-    if (localStorage.getItem('auth-token')) {
-        const profileurl = import.meta.env.VITE_PROFILE_URL;
+        if (localStorage.getItem('auth-token')) {
+            const profileurl = import.meta.env.VITE_PROFILE_URL;
 
-        const response = await fetch(profileurl, {
-            method: 'POST',
-            headers: {
-                'auth-token': localStorage.getItem('auth-token')
-            }
-        })
-        const jsonprofileresponse = await response.json();
-        setUsername(jsonprofileresponse.name);
-        setUseremail(jsonprofileresponse.email);
+            const response = await fetch(profileurl, {
+                method: 'POST',
+                headers: {
+                    'auth-token': localStorage.getItem('auth-token')
+                }
+            })
+            const jsonprofileresponse = await response.json();
+            setUsername(jsonprofileresponse.name);
+            setUseremail(jsonprofileresponse.email);
 
 
-    } else {
-        navigate('/Signin')
-    }
+        } else {
+            navigate('/Signin')
+        }
+    }, [])
 
     return (
         <div className='profilecontainer'>
