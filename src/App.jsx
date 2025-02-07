c48ee0967e585e5f2d762bea8ff9608605e2c680
Bharatesh Poojary <bharateshpoojari@gmail.com>
2025-02-07 22:44:04 +0530

    test done

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ const App = () => {
     return (
         <div>
             <NoteState>{/*3 . NoteState is the component to which we are passing navbar and outlet as prop. Wrapping up this component so that it can access the  contexts state */}
-                <Outlet />
                 <Navbar />
+                <Outlet />
                 {/* tells react router where to render child components inside a parent route here our parent route is App  */}
             </NoteState>
         </div>
